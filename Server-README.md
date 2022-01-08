17ce83914083ea94ee08a28ed3df0dd76469251c
Henning 'haenno' Beier <haenno@web.de>
2022-01-08 20:13:29 +0100

    update tools

--- Server-README.md
+++ Server-README.md
@@ -10,7 +10,7 @@ Henning 'haenno' Beier, haenno@web.de, 24.11.2021
 ## Automation of git repo updates to this servers containers
 
 With the 'server-update-cron.sh' script in the **crontab** as...
-``*/5 * * * * /home/rjhadmin/desksahre/server-update-cron.sh >> /home/rjhadmin/deskshare/cronlog.txt 2>&1``
+``*/5 * * * * /home/rjhadmin/desksahre/server-update-cron.sh >> /home/rjhadmin/deskshare_cronlog.txt 2>&1``
 
 ...this server automaticly pulls each new commit to this repo, and restarts the docker containers.
 
--- server-update-cron.sh
+++ server-update-cron.sh
@@ -3,19 +3,16 @@
 cd /home/rjhadmin/deskshare/
 now=$(date "+%F %H:%M:%S")
 # echo "$now --> deskshare update > Starting..."
-git -C /home/rjhadmin/deskshare/ fetch origin >> /home/rjhadmin/deskshare/cronlog.txt 
+git -C /home/rjhadmin/deskshare/ fetch origin >> /home/rjhadmin/deskshare_cronlog.txt 
 if  [ `git -C /home/rjhadmin/deskshare/ rev-list HEAD...origin/master --count` != 0 ] 
 then
     echo "$now --> deskshare update > Remote git repo newer > Updating..."
-    docker-compose --project-directory /home/rjhadmin/deskshare/ stop deskshare  >> /home/rjhadmin/deskshare/cronlog.txt
-    git -C /home/rjhadmin/deskshare/ reset --hard origin/master  >> /home/rjhadmin/deskshare/cronlog.txt
-    git -C /home/rjhadmin/deskshare/ fetch >> /home/rjhadmin/deskshare/cronlog.txt
-    git -C /home/rjhadmin/deskshare/ pull >> /home/rjhadmin/deskshare/cronlog.txt
-    chmod +x /home/rjhadmin/deskshare/*.sh >> /home/rjhadmin/deskshare/cronlog.txt
-    docker-compose --project-directory /home/rjhadmin/deskshare/ run deskshare python rpg/manage.py makemigrations >> /home/rjhadmin/deskshare/cronlog.txt
-    docker-compose --project-directory /home/rjhadmin/deskshare/ run deskshare python rpg/manage.py migrate >> /home/rjhadmin/deskshare/cronlog.txt
-    docker-compose --project-directory /home/rjhadmin/deskshare/ run deskshare python rpg/manage.py loaddata db_sample_data.json >> /home/rjhadmin/deskshare/cronlog.txt
-    docker-compose --project-directory /home/rjhadmin/deskshare/ start deskshare >> /home/rjhadmin/deskshare/cronlog.txt
+    docker-compose --project-directory /home/rjhadmin/deskshare/ stop  >> /home/rjhadmin/deskshare_cronlog.txt
+    git -C /home/rjhadmin/deskshare/ reset --hard origin/master  >> /home/rjhadmin/deskshare_cronlog.txt
+    git -C /home/rjhadmin/deskshare/ fetch >> /home/rjhadmin/deskshare_cronlog.txt
+    git -C /home/rjhadmin/deskshare/ pull >> /home/rjhadmin/deskshare_cronlog.txt
+    chmod +x /home/rjhadmin/deskshare/*.sh >> /home/rjhadmin/deskshare_cronlog.txt
+    docker-compose --project-directory /home/rjhadmin/deskshare/ start >> /home/rjhadmin/deskshare_cronlog.txt
     # echo "$now --> deskshare update > Update done!"
 else
     echo "$now --> deskshare update > Remote git repo same as local > Nothing to do!"
