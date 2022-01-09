4eaa978c744dc72b6e67f05d5cec7da29db961ee
haenno <haenno@web.de>
2022-01-09 15:28:32 +0100

    update tools

--- Server-README.md
+++ Server-README.md
@@ -9,12 +9,8 @@ Henning 'haenno' Beier, haenno@web.de, 08.01.2022
 
 ## Automation of git repo updates to this servers containers
 
-With scripts in the **crontab** this server automaticly pulls each new commit to this repo, and restarts the docker containers every 5 minutes. Every 30 minutes it performs a complete rebuild.
+With scripts in the **crontab** this server automaticly pulls each new commit to this repo, and restarts the docker containers every 5 minutes.
 
 ### Update
 
 ``*/5 * * * * /home/rjhadmin/deskshare/server-update-cron.sh >> /home/rjhadmin/deskshare_cronlog.txt 2>&1``
-
-### Rebuild
-
-``*/30 * * * * /home/rjhadmin/deskshare/server-rebuild.sh >> /home/rjhadmin/deskshare_cronlog.txt 2>&1``
--- server-update-cron.sh
+++ server-update-cron.sh
@@ -12,6 +12,8 @@ then
     git -C /home/rjhadmin/deskshare/ fetch >> /home/rjhadmin/deskshare_cronlog.txt
     git -C /home/rjhadmin/deskshare/ pull >> /home/rjhadmin/deskshare_cronlog.txt
     chmod +x /home/rjhadmin/deskshare/*.sh >> /home/rjhadmin/deskshare_cronlog.txt
+    docker-compose --project-directory /home/rjhadmin/deskshare/ build api >> /home/rjhadmin/deskshare_cronlog.txt
+    docker-compose --project-directory /home/rjhadmin/deskshare/ build client >> /home/rjhadmin/deskshare_cronlog.txt
     docker-compose --project-directory /home/rjhadmin/deskshare/ up -d >> /home/rjhadmin/deskshare_cronlog.txt
     docker-compose --project-directory /home/rjhadmin/deskshare/ stop api >> /home/rjhadmin/deskshare_cronlog.txt
     docker-compose --project-directory /home/rjhadmin/deskshare/ up -d api >> /home/rjhadmin/deskshare_cronlog.txt    
